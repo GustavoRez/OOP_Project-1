d4db313f5e04a2f7aebde74985b289b87993bf06
Gustavo <110946102+GustavoRez@users.noreply.github.com>
2024-04-23 16:47:02 -0300

    Add files via upload

---
+++ Tasks_6/src/java/me/gustavo.java
@@ -0,0 +1,75 @@
+package me;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/gustavo.json"})
+public class gustavo extends HttpServlet {
+
+        protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            out.println("<title> Dados do Aluno </title>");
+            out.println("<h2>RA: 1290482222036</h2>");            
+            out.println("<h2>Nome: Gustavo de Rezende Garcia</h2>");
+            
+            String mat[] = {"Sistemas Operacionais II", "POO", "BD", "Engenharia de Software III", "Gestão de Projetos"};
+            out.println("<br>");
+            out.println("<h2>Matérias cursadas</h2>");
+            
+            for(int i = 0; i < mat.length; i++){
+                out.println("<ul>");
+                out.println("<li>" + mat[i]);
+                out.println("</ul>");
+            }
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
